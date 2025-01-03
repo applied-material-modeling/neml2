ebb539421da461483ddd53af2d1bc9eb440ebb47
hugary1995 <thu@anl.gov>
2025-01-03 19:04:48 +0000

    Deploying to gh-pages from @ applied-material-modeling/neml2@0e05ff17c9a81b15e9c7d1c1c3ef5dbb66b1406e 🚀

---
+++ pr-preview/pr-190/navtreedata.js
@@ -0,0 +1,98 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "NEML2", "index.html", [
+    [ "Overview", "index.html#autotoc_md1", [
+      [ "The New Engineering Material model Library, version 2", "index.html#autotoc_md2", null ],
+      [ "Quick installation", "index.html#autotoc_md3", null ],
+      [ "Features and design philosophy", "index.html#autotoc_md4", null ]
+    ] ],
+    [ "Installation Guide", "install.html", null ],
+    [ "User Guide", "usergroup0.html", [
+      [ "Input File", "input-file.html", null ],
+      [ "C++ Backend", "cpp-backend.html", null ],
+      [ "Python Package", "python-package.html", null ]
+    ] ],
+    [ "Developer Guide", "usergroup1.html", [
+      [ "Development Environment", "dev-env.html", null ],
+      [ "Naming Conventions", "naming-conventions.html", null ],
+      [ "Custom Model", "custom-model.html", null ],
+      [ "Testing", "testing.html", null ],
+      [ "Model Visualization", "model-visualization.html", null ]
+    ] ],
+    [ "Syntax Documentation", "usergroup2.html", [
+      [ "[Settings]", "syntax-settings.html", null ],
+      [ "[Tensors]", "syntax-tensors.html", null ],
+      [ "[Models]", "syntax-models.html", null ],
+      [ "[Solvers]", "syntax-solvers.html", null ],
+      [ "[Data]", "syntax-data.html", null ],
+      [ "[Drivers]", "syntax-drivers.html", null ]
+    ] ],
+    [ "System Documentation", "usergroup3.html", [
+      [ "Settings", "system-settings.html", null ],
+      [ "Tensor", "system-tensors.html", null ],
+      [ "Model", "system-models.html", null ],
+      [ "Solver", "system-solvers.html", null ],
+      [ "Data", "system-data.html", null ],
+      [ "Driver", "system-drivers.html", null ]
+    ] ],
+    [ "Physics Documentation", "usergroup4.html", [
+      [ "Solid Mechanics", "solid-mechanics.html", null ],
+      [ "Liquid Infiltration", "liquid-infiltration.html", null ]
+    ] ],
+    [ "Frequently Asked Questions", "faq.html", null ],
+    [ "C++ API Reference", "usergroup5.html", [
+      [ "Namespaces", "namespaces.html", "namespaces" ],
+      [ "Classes", "annotated.html", "annotated" ]
+    ] ],
+    [ "Python API Reference", "usergroup6.html", [
+      [ "Namespaces", "python/namespaces.html", null ],
+      [ "Classes", "python/annotated.html", null ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classneml2_1_1DependencyResolver.html#a1588d1db10fac8b654dbe96e5b2f1d33",
+"classneml2_1_1FixOrientation.html#a577b5a402c45c2205612b9446b9bfbff",
+"classneml2_1_1KocksMeckingActivationEnergy.html#a934b3165d862dea20388c4e655fa7c4d",
+"classneml2_1_1LabeledAxisAccessor.html#aeb286c8f7d0bf903ca071c65be12a2b7",
+"classneml2_1_1Model.html#ae9844a8a07579e1cb5de769b604ff375",
+"classneml2_1_1OlevskySinteringStress.html#ae21798880bc558d25d7073e9ba07cc4a",
+"classneml2_1_1Parser.html#acbc8f3a808768ac0744112e1be36d064",
+"classneml2_1_1R4.html#acd44def43269b1f458acd7e6ff0f01a2",
+"classneml2_1_1SolidMechanicsDriver.html#a32d3f95b4b6ad0ee3d83b2fe037589ca",
+"classneml2_1_1Transformable.html#a2015a73dabd206a9a68a209bceaae958",
+"classneml2_1_1VariableBase.html#aa9705fe094f6261829017ddc444d2343",
+"classneml2_1_1WR2ImplicitExponentialTimeIntegration.html#aec4b2bcafb1752418187d36995ede628",
+"namespaceneml2.html#a6f62d77d8dd6cdf23c2b96a4b2021d5d",
+"namespaceneml2_1_1utils.html#a762c72f1cb58dcffa8faf21111e62086"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
