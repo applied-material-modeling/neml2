d5396dd5849a2bcee569bc5e5fefd33e875660af
Gary Hu <thu@anl.gov>
2025-03-27 08:35:33 -0500

    Allow test suites to specify additional devices
    Add a cliarg `-d`/`--devices` to specify a list of additional (non-CPU) devices that the test suite can use.
    
    ModelUnitTest now loops through this list to repeat all unit tests.

--- tests/regression/main.cxx
+++ tests/regression/main.cxx
@@ -22,37 +22,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#include <catch2/catch_session.hpp>
-
-#include <filesystem>
 #include "utils.h"
 
 int
 main(int argc, char * argv[])
 {
-  Catch::Session session;
-
-  // Add path cli arg
-  using namespace Catch::Clara;
-  std::string working_dir;
-  auto cli = session.cli() | Opt(working_dir, ".")["-p"]["--path"]("path to the test input files");
-  session.cli(cli);
-
-  // Let Catch2 parse the command line
-  auto err = session.applyCommandLine(argc, argv);
-  if (err)
-    return err;
-
-  // Set the working directory
-  auto exec_prefix = std::filesystem::path(argv[0]).parent_path();
-  err = guess_test_dir("regression", working_dir, exec_prefix);
-  if (err)
-    return err;
-  std::cout << "Working directory: " << working_dir << std::endl;
-  std::filesystem::current_path(working_dir);
-
-  // Set default tensor options
-  neml2::set_default_dtype(neml2::kFloat64);
-
-  return session.run();
+  return test_main(argc, argv, "regression");
 }
