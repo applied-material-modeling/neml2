5344b31f57605b22fad46b8cb99a91507c0c14de
hugary1995 <hugary1995@users.noreply.github.com>
2025-04-03 00:11:11 +0000

    Deploying to gh-pages from @ applied-material-modeling/neml2@e9dcd1d633c7a6a6b071e7eff3db7f4ee2334ee4 🚀

---
+++ pr-preview/pr-243/navtreedata.js
@@ -0,0 +1,130 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "NEML2", "index.html", [
+    [ "Overview", "index.html#autotoc_md1", [
+      [ "The New Engineering Material model Library, version 2", "index.html#autotoc_md2", null ],
+      [ "Quick installation", "index.html#autotoc_md3", null ],
+      [ "Features and design philosophy", "index.html#autotoc_md4", null ]
+    ] ],
+    [ "Installation", "usergroup0.html", [
+      [ "Basic Installation", "install.html", null ],
+      [ "Dependency Management", "dependency-management.html", null ],
+      [ "Build Customization", "build-customization.html", null ],
+      [ "External Project Integration", "external-project-integration.html", null ]
+    ] ],
+    [ "Guides and Tutorials", "tutorials.html", [
+      [ "Getting Started", "tutorials-getting-started.html", null ],
+      [ "Contributing", "tutorials-contributing.html", null ],
+      [ "Models", "tutorials-models.html", [
+        [ "Input file", "tutorials-models-input-file.html", null ],
+        [ "Running your first model", "tutorials-models-running-your-first-model.html", null ],
+        [ "Model parameters", "tutorials-models-model-parameters.html", null ],
+        [ "Evaluation device", "tutorials-models-evaluation-device.html", null ],
+        [ "Vectorization", "tutorials-models-vectorization.html", null ],
+        [ "Model composition", "tutorials-models-model-composition.html", null ],
+        [ "Model parameters (revisited)", "tutorials-models-model-parameters-revisited.html", null ],
+        [ "Implicit model", "tutorials-models-implicit-model.html", null ],
+        [ "Transient driver", "tutorials-models-transient-driver.html", null ],
+        [ "Just-in-time compilation", "tutorials-models-just-in-time-compilation.html", null ]
+      ] ],
+      [ "Tensors", "tutorials-tensors.html", [
+        [ "Tensor types", "tutorials-tensors-tensor-types.html", null ],
+        [ "Tensor creation", "tutorials-tensors-tensor-creation.html", null ],
+        [ "Indexing", "tutorials-tensors-indexing.html", null ],
+        [ "Tensor view", "tutorials-tensors-tensor-view.html", null ],
+        [ "Broadcasting", "tutorials-tensors-broadcasting.html", null ]
+      ] ],
+      [ "Extension", "tutorials-extension.html", [
+        [ "Connection to input files", "tutorials-extension-connection-to-input-files.html", null ],
+        [ "Argument declaration", "tutorials-extension-argument-declaration.html", null ],
+        [ "The forward operator", "tutorials-extension-the-forward-operator.html", null ],
+        [ "Model composition", "tutorials-extension-model-composition.html", null ],
+        [ "Model visualization", "tutorials-extension-model-visualization.html", null ]
+      ] ],
+      [ "Optimization", "tutorials-optimization.html", [
+        [ "Automatic differentiation", "tutorials-optimization-automatic-differentiation.html", null ],
+        [ "Parameter calibration", "tutorials-optimization-parameter-calibration.html", null ]
+      ] ],
+      [ "Next steps", "tutorials-next-steps.html", null ]
+    ] ],
+    [ "Syntax Documentation", "usergroup1.html", [
+      [ "[Settings]", "syntax-settings.html", null ],
+      [ "[Tensors]", "syntax-tensors.html", null ],
+      [ "[Models]", "syntax-models.html", null ],
+      [ "[Solvers]", "syntax-solvers.html", null ],
+      [ "[Data]", "syntax-data.html", null ],
+      [ "[Drivers]", "syntax-drivers.html", null ],
+      [ "[Schedulers]", "syntax-schedulers.html", null ]
+    ] ],
+    [ "System Documentation", "usergroup2.html", [
+      [ "Settings", "system-settings.html", null ],
+      [ "Tensor", "system-tensors.html", null ],
+      [ "Model", "system-models.html", null ],
+      [ "Solver", "system-solvers.html", null ],
+      [ "Data", "system-data.html", null ],
+      [ "Driver", "system-drivers.html", null ],
+      [ "Scheduler", "system-schedulers.html", null ]
+    ] ],
+    [ "Physics Documentation", "usergroup3.html", [
+      [ "Solid Mechanics", "solid-mechanics.html", null ],
+      [ "Reactive Infiltration", "reactive-infiltration.html", null ]
+    ] ],
+    [ "Aditional Documentation", "topics.html", "topics" ],
+    [ "C++ API Reference", "usergroup4.html", [
+      [ "Namespaces", "namespaces.html", "namespaces" ],
+      [ "Classes", "annotated.html", "annotated" ]
+    ] ],
+    [ "Python API Reference", "usergroup5.html", [
+      [ "Namespaces", "python/namespaces.html", null ],
+      [ "Classes", "python/annotated.html", null ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classneml2_1_1DependencyResolver.html#a201b2fcc14bade0611c6d90841748bec",
+"classneml2_1_1FischerBurmeister.html#a2fbeddd7dfb0359118b4823809f597ad",
+"classneml2_1_1IsotropicHardeningStaticRecovery.html",
+"classneml2_1_1LabeledAxisAccessor.html#a25fea79e63e6e8aac92b06df627bc63c",
+"classneml2_1_1Model.html#a86340fc1c69334362e2c480a738ca44c",
+"classneml2_1_1NonlinearSystem.html#a8a59b787a2cd9f3fafbac37e158ffa12",
+"classneml2_1_1ParameterStore.html",
+"classneml2_1_1R2.html#a8f7439589671fcd9d7bbca76a167b371",
+"classneml2_1_1SSR4.html#a1cafd07e537a6fa128b6e46d8d05883a",
+"classneml2_1_1TensorBase.html#a4b7be101bdff3388fd0e4bc34fdd26cf",
+"classneml2_1_1ValueMapLoader.html#acaeaede59f971eb6cd4567dfaf037a01",
+"classneml2_1_1VariableStore.html#aa7da83ed703c21222cb74f6e846e33fb",
+"classneml2_1_1WorkGenerator.html#adb517005f724bd6a29bbc3c5b53d9cde",
+"namespaceneml2.html#a45857029420659268bda43f87f148410",
+"namespaceneml2.html#ac52c1c5e394782f1ad9c3642629b64dd",
+"structneml2_1_1NonlinearSolver_1_1Result.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
---
+++ pr-preview/pr-243/search/typedefs_6.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['r2backwardeulertimeintegration_0',['R2BackwardEulerTimeIntegration',['../namespaceneml2.html#a982ea53efecd11e9e22b44e76d142bc8',1,'neml2']]],
+  ['r2incrementtorate_1',['R2IncrementToRate',['../namespaceneml2.html#a910a1045bc2a18cbbdfdebe3ba903270',1,'neml2']]],
+  ['real_2',['Real',['../namespaceneml2.html#a4b04262b81aa7d31eb5d2f607e2a35de',1,'neml2']]],
+  ['resulttype_3',['ResultType',['../classneml2_1_1ElasticityConverter.html#aa47bc091f51f2e0d52fcbcfc9cdc584e',1,'neml2::ElasticityConverter']]]
+];
