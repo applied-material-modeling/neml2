7045af4b4adee01e242aaccf050e0b346d1f920a
hugary1995 <hugary1995@users.noreply.github.com>
2025-07-03 13:14:36 +0000

    Deploying to gh-pages from @ applied-material-modeling/neml2@156229a9ff2bd015b63ef8201f7465f37fdd2faa 🚀

---
+++ pr-preview/pr-268/navtreedata.js
@@ -0,0 +1,137 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "NEML2", "index.html", [
+    [ "Overview", "index.html#autotoc_md1", [
+      [ "The New Engineering Material model Library, version 2", "index.html#autotoc_md2", null ],
+      [ "Quick installation", "index.html#autotoc_md3", null ],
+      [ "Features and design philosophy", "index.html#autotoc_md4", null ],
+      [ "Citing NEML2", "index.html#autotoc_md5", null ]
+    ] ],
+    [ "Installation", "usergroup0.html", [
+      [ "Basic Installation", "install.html", null ],
+      [ "Dependency Management", "dependency-management.html", null ],
+      [ "Build Customization", "build-customization.html", null ],
+      [ "External Project Integration", "external-project-integration.html", null ]
+    ] ],
+    [ "Guides and Tutorials", "tutorials.html", [
+      [ "Getting Started", "tutorials-getting-started.html", null ],
+      [ "Contributing", "tutorials-contributing.html", null ],
+      [ "Models", "tutorials-models.html", [
+        [ "Input file", "tutorials-models-input-file.html", null ],
+        [ "Running your first model", "tutorials-models-running-your-first-model.html", null ],
+        [ "Model parameters", "tutorials-models-model-parameters.html", null ],
+        [ "Evaluation device", "tutorials-models-evaluation-device.html", null ],
+        [ "Vectorization", "tutorials-models-vectorization.html", null ],
+        [ "Model composition", "tutorials-models-model-composition.html", null ],
+        [ "Model parameters (revisited)", "tutorials-models-model-parameters-revisited.html", null ],
+        [ "Implicit model", "tutorials-models-implicit-model.html", null ],
+        [ "Transient driver", "tutorials-models-transient-driver.html", null ],
+        [ "Just-in-time compilation", "tutorials-models-just-in-time-compilation.html", null ]
+      ] ],
+      [ "Tensors", "tutorials-tensors.html", [
+        [ "Tensor types", "tutorials-tensors-tensor-types.html", null ],
+        [ "Tensor creation", "tutorials-tensors-tensor-creation.html", null ],
+        [ "Indexing", "tutorials-tensors-indexing.html", null ],
+        [ "Tensor view", "tutorials-tensors-tensor-view.html", null ],
+        [ "Broadcasting", "tutorials-tensors-broadcasting.html", null ]
+      ] ],
+      [ "Extension", "tutorials-extension.html", [
+        [ "Connection to input files", "tutorials-extension-connection-to-input-files.html", null ],
+        [ "Argument declaration", "tutorials-extension-argument-declaration.html", null ],
+        [ "The forward operator", "tutorials-extension-the-forward-operator.html", null ],
+        [ "Model composition", "tutorials-extension-model-composition.html", null ],
+        [ "Model visualization", "tutorials-extension-model-visualization.html", null ]
+      ] ],
+      [ "Optimization", "tutorials-optimization.html", [
+        [ "Automatic differentiation", "tutorials-optimization-automatic-differentiation.html", null ],
+        [ "Parameter calibration", "tutorials-optimization-parameter-calibration.html", null ]
+      ] ],
+      [ "Next steps", "tutorials-next-steps.html", null ]
+    ] ],
+    [ "Syntax Documentation", "usergroup1.html", [
+      [ "[Settings]", "syntax-settings.html", null ],
+      [ "[Tensors]", "syntax-tensors.html", null ],
+      [ "[Models]", "syntax-models.html", null ],
+      [ "[Solvers]", "syntax-solvers.html", null ],
+      [ "[Data]", "syntax-data.html", null ],
+      [ "[Drivers]", "syntax-drivers.html", null ],
+      [ "[Schedulers]", "syntax-schedulers.html", null ]
+    ] ],
+    [ "System Documentation", "usergroup2.html", [
+      [ "Settings", "system-settings.html", null ],
+      [ "Tensor", "system-tensors.html", null ],
+      [ "Model", "system-models.html", null ],
+      [ "Solver", "system-solvers.html", null ],
+      [ "Data", "system-data.html", null ],
+      [ "Driver", "system-drivers.html", null ],
+      [ "Scheduler", "system-schedulers.html", null ]
+    ] ],
+    [ "Physics Documentation", "usergroup3.html", [
+      [ "Solid Mechanics", "solid-mechanics.html", null ],
+      [ "Chemical Reactions", "chemical-reactions.html", [
+        [ "Thermosetting Pyrolysis", "thermosetting-pyrolysis.html", null ],
+        [ "Reactive Infiltration", "reactive-infiltration.html", null ]
+      ] ],
+      [ "Phase-Field Fracture", "phase-field-fracture.html", null ]
+    ] ],
+    [ "Aditional Documentation", "topics.html", "topics" ],
+    [ "C++ API Reference", "usergroup4.html", [
+      [ "Namespaces", "namespaces.html", "namespaces" ],
+      [ "Classes", "annotated.html", "annotated" ]
+    ] ],
+    [ "Python API Reference", "usergroup5.html", [
+      [ "Namespaces", "python/namespaces.html", null ],
+      [ "Classes", "python/annotated.html", null ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classneml2_1_1CrackGeometricFunctionAT2.html",
+"classneml2_1_1Elasticity.html#a7872d44604858df3de72bdf95eeb4f83",
+"classneml2_1_1FullTensor.html",
+"classneml2_1_1KocksMeckingFlowViscosity.html#a0cece6f2b9637d96e358e1e9a4a73566",
+"classneml2_1_1LinearCombination.html",
+"classneml2_1_1Model.html#adb71c578142121b35a1979c9328a9572",
+"classneml2_1_1NonlinearSystem.html#abee34d5b44ad17d75366b861d6d96087",
+"classneml2_1_1ParameterStore.html#ac44196489c29d0a5f8e1c6d6399d6b4e",
+"classneml2_1_1R2Base.html#aa889d1db4bb3b8d9dc856c52c802d067",
+"classneml2_1_1SSR4.html#a445d80b775b25ed5357f049fc861754f",
+"classneml2_1_1SwellingAndPhaseChangeDeformationJacobian.html#a11ea402a01125e7fb50795fa317ecbfb",
+"classneml2_1_1TransientDriver.html#a04670c77a18422a80c4c62c2194ea098",
+"classneml2_1_1VariableBase.html#ab7bfd4b8325b48540744cd5a95a6acae",
+"classneml2_1_1WR2ExplicitExponentialTimeIntegration.html#a55ad50232e5744e670a37cd2fd8c0e47",
+"namespaceneml2.html#a094044d18ca027327ae9687ecbe8dbcd",
+"namespaceneml2.html#a7e971184c15ef911b2102983a8271c58",
+"namespaceneml2.html#affb3c259a6d78638464993365f251e4b",
+"structneml2_1_1TensorCache.html#ae57fd65041c9dc727c27e8645fe0d813"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
