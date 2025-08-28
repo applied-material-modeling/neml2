9ed7c12dba8f51785734af383b93af8874d49ec3
Wendy-Ji <j.wendy234@gmail.com>
2025-08-29 01:08:39 +1000

    update for changes to main branch

--- src/neml2/user_tensors/CSVPrimitiveTensor.cxx
+++ src/neml2/user_tensors/CSVPrimitiveTensor.cxx
@@ -91,7 +91,6 @@ CSVPrimitiveTensor<Vec>::parse_csv(const std::string & csv_file,
   csv::CSVReader reader(csv_file);
 
   std::vector<Vec> csv_vals;
-
   std::vector<std::string> suffix = {"_x", "_y", "_z"};
 
   for (const auto & row : reader)
