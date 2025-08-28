2dcddd53bd1ff515b159c3f06724bd8ae8379fbe
Wendy-Ji <j.wendy234@gmail.com>
2025-08-29 01:05:07 +1000

    primitive tensor initial

--- tests/unit/user_tensors/test_CSVPrimitiveTensor.cxx
+++ tests/unit/user_tensors/test_CSVPrimitiveTensor.cxx
@@ -42,13 +42,20 @@ TEST_CASE("CSVPrimitiveTensor", "[user_tensors]")
     REQUIRE(a->batch_sizes() == TensorShape{4});
     REQUIRE(a->base_sizes() == Scalar::const_base_sizes);
 
-    const auto b = factory->get_object<SR2>("Tensors", "b");
-    const auto b_correct = SR2::create({{0.5, 1.1, 2.2, 0.5, 1.1, 2.2},
+    const auto b = factory->get_object<Vec>("Tensors", "b");
+    const auto b_correct =
+        Vec::create({{0.0, 0.5, 0.4}, {0.1, -0.1, 0.0}, {0.2, 0.0, 0.2}, {0.3, 0.2, -0.1}});
+    REQUIRE(at::allclose(*b, b_correct));
+    REQUIRE(b->batch_sizes() == TensorShape{4});
+    REQUIRE(b->base_sizes() == Vec::const_base_sizes);
+
+    const auto c = factory->get_object<SR2>("Tensors", "c");
+    const auto c_correct = SR2::create({{0.5, 1.1, 2.2, 0.5, 1.1, 2.2},
                                         {-1.5, 2.2, 3.3, 0.5, 1.1, 2.2},
                                         {-3.0, -5.0, 100.1, 0.5, 1.1, 2.2},
                                         {20.1, -1.1, -13.0, 0.5, 1.1, 2.2}});
-    REQUIRE(at::allclose(*b, b_correct));
-    REQUIRE(b->batch_sizes() == TensorShape{4});
-    REQUIRE(b->base_sizes() == SR2::const_base_sizes);
+    REQUIRE(at::allclose(*c, c_correct));
+    REQUIRE(c->batch_sizes() == TensorShape{4});
+    REQUIRE(c->base_sizes() == SR2::const_base_sizes);
   }
 }
--- tests/unit/user_tensors/test_CSVPrimitiveTensor.i
+++ tests/unit/user_tensors/test_CSVPrimitiveTensor.i
@@ -5,10 +5,16 @@
     tensor_name = 'scalar'
     batch_shape = '(4)'
   []
-#   [b]
-#     type = CSVSR2
-#     csv_file = 'user_tensors/CSVPrimitiveTensor.csv'
-#     tensor_name = 's'
-#     batch_shape = '(4)'
-#   []
+  [b]
+    type = CSVVec
+    csv_file = 'user_tensors/CSVPrimitiveTensor.csv'
+    tensor_name = 'disp'
+    batch_shape = '(4)'
+  []
+  [c]
+    type = CSVSR2
+    csv_file = 'user_tensors/CSVPrimitiveTensor.csv'
+    tensor_name = 's'
+    batch_shape = '(4)'
+  []
 []
