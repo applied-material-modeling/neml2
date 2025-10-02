c40d2fe237099f1a3bc8167857863ee6b31cdfd6
zaheen1258 <zaheen1258@users.noreply.github.com>
2025-10-02 16:25:15 +0000

    Deploying to gh-pages from @ applied-material-modeling/neml2@247e1a6dd9e9f9c3b24e4ca13890c3187ee97cfa 🚀

---
+++ pr-preview/pr-296/search/variables_2.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['b_0',['b',['../namespaceneml2_1_1crystallography_1_1crystal__symmetry__operators.html#aa93a189fb7bbbf87be54bcc7bd11bb6f',1,'neml2::crystallography::crystal_symmetry_operators']]],
+  ['batch_5fdims_1',['batch_dims',['../structneml2_1_1Model_1_1TraceSchema.html#a6e8a5c7cfb5ee2b5a176d58a8a91566d',1,'neml2::Model::TraceSchema']]],
+  ['batch_5fsize_2',['batch_size',['../structneml2_1_1StaticHybridScheduler_1_1DeviceStatus.html#a76568f46e992a759cb85af59a708e814',1,'neml2::StaticHybridScheduler::DeviceStatus']]],
+  ['build_3',['build',['../structneml2_1_1NEML2ObjectInfo.html#a9232c3e201ab186c11087ab12680eded',1,'neml2::NEML2ObjectInfo']]]
+];
